da720ff376ccfe9b2d62185fdb52008950008361
Marc Durham <marcdurham@gmail.com>
2018-11-04 22:21:59 +0800

    Update Deploy example script

--- Deploy.Example.ps1
+++ Deploy.Example.ps1
@@ -1,22 +1,17 @@
 ﻿Import-Module AWSPowerShell
 Import-Module .\ClickOnce-Module.ps1
 
-#Build project
-"Building..."
-msbuild.exe AppName.WinForms.csproj /p:Configuration=Release /p:TargetVersion=4.0
+Write-Host "Building Project..."
+msbuild.exe AppName.WinForms.csproj /p:Configuration=Release
 
-pushd .\
-cd ".\bin\Release"
-
-""
+Write-Host ""
 # Optional Extra Commands Here
-# "Compressing UserManual folder..."
+# Write-Host "Compressing UserManual folder..."
 # 7z a -sfx UserManual.exe UserManual
 
-""
-"Configuring files to deploy..." 
-#Configure file paths 
-#The first file is the "Entry Point" file, the main .exe.
+Write-Host ""
+Write-Host "Preparing list of files to publish..." 
+# The first file is the "Entry Point" file, your main .exe.
 $files = `
     "AppName.exe", `
     "AppName.pdb", `
@@ -26,20 +21,48 @@ $files = `
     "AppName.Common.dll", `
     "AppName.Common.pdb"
 
-"Creating ClickOnce manifest files..." 
-Create-ClickOnce $files `
-    -AppLongName "Full Application Name" `
-    -AppShortName "AppName" `
-    -IconFile "AppNameIcon.ico" `
-    -Publisher "Your Name" `
-    -OutputDir "../../../../ClickOnceDeploy" `
-    -CertFile "../../../AppName.pfx" `
-    -DeploymentRootUrl "http://appname.md9.us" `
-    -AmazonRegion "us-west-2" `
-    -AmazonS3BucketName "appname.md9.us" `
-    -FileExt ".abc" `
-    -FileExtDescription "Application Name ABC File" `
-    -FileExtProgId "AppName.ABC" `
-    -ErrorAction Stop
+try {
+    Write-Host "Publish ClickOnce files..." 
+    $publishedPaths = Publish-ClickOnce `
+        -Files $files `
+        -AppLongName "Full Application Name" `
+        -AppShortName "AppName" `
+        -IconFile "YourIconFile.ico" `
+        -Publisher "Your Name" `
+        -OutputDir "/YourLocalClickOncePublishFolder" `
+        -CertFile "../../YourPrivateKeyFile.pfx" `
+        -DeploymentRootUrl "http://downloads.domain.com/appname" `
+        -FileExtension ".abc" `
+        -FileExtDescription "Application Name ABC File" `
+        -FileExtProgId "AppName.ABC" `
+        -ErrorAction Stop
+        
+    if ($publishedPaths.Count -eq 0) {
+        Write-Host "Error.  No files were published.  Nothing to upload." `
+            -ForegroundColor Red
+        popd
+        return
+    }
+
+    Write-Host "Uploading published files to Amazon S3..."
+    foreach ($path in $publishedPaths) {
+        Write-Host $path -NoNewline
+        Write-S3Object `
+            -BucketName  "bucket.name.here" `
+            -Region  "us-west-2" `
+            -File $path `
+            -Key "$($path)" `
+            -CannedACLName public-read `
+            -ErrorAction Stop
+        Write-Host " DONE" -ForegroundColor Green
+    }
+
+    Write-Host "Finished uploading to Amazon S3." -ForegroundColor Green
+} catch { 
+    Write-Host "ERROR" -ForegroundColor Red
+    Write-Host $Error -ForegroundColor Red
+    popd
+    return
+}
 
 popd
