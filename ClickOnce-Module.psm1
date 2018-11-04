94b4f07e534c3d349e5191ffa4bdabc1d120a3f7
Marc Durham <marcdurham@gmail.com>
2018-11-04 19:48:19 +0800

    Write output to host, improve messages

--- ClickOnce-Module.psm1
+++ ClickOnce-Module.psm1
@@ -27,26 +27,38 @@ function Publish-ClickOnce {
         $FileExtDescription, `
         $FileExtProgId) 
 
-    Write-Verbose "Creating ClickOnce deployment for $AppLongName..."
+    Write-Host "Creating ClickOnce deployment for $AppLongName..."
 
-    Write-Verbose "Peparing $($Files.Count) Files to Deploy:"
-    foreach ($f in $Files) {
-        Write-Verbose "    $f"
-    }
-
-    Write-Verbose "The first file, at index 0, will be the ""Entry Point"" file, or the main .exe."
-
-    Write-Verbose "Getting last revision from Revision.txt..."
-    $revisionString = Get-Content "./Revision.txt"
-    $revision = [Int32]::Parse($revisionString) + 1
-    $version = "1.0.0.$revision"
-    Write-Verbose "Deploying as version: $version"
-
-    Write-Verbose "Moving to binary release folder..."
+    Write-Host "Changing current directory to binary release folder..."
     pushd .\
     cd ".\bin\Release"
+
+    $missing = 0
+    Write-Host "Peparing $($Files.Count) files for deployment:"
+    foreach ($f in $Files) {
+        Write-Host "    $f" -NoNewline
+        if (Test-Path $f) {
+            Write-Host " EXISTS" -ForegroundColor Green
+        } else {
+            $missing += 1
+            Write-Host " MISSING" -ForegroundColor Red
+        }
+    }
+
+    if ($missing -gt 0) {
+        Write-Host "$missing files missing. Aborting script." -ForegroundColor Red
+        return
+    }
+
+    Write-Host "The first file, at index 0, will be the ""Entry Point"" file, or the main .exe."
+
+    Write-Host "Getting last revision from Revision.txt..."
+    $revisionString = Get-Content "../../Revision.txt"
+    $revision = [Int32]::Parse($revisionString) + 1
+    $version = "1.0.0.$revision"
+    Write-Host "Deploying as version: $version"
     
-    Write-Verbose "Current Folder: $(Get-Location)"
+    Write-Host "Current Folder: $(Get-Location)"
     
     $appManifest = "$AppShortName.exe.manifest"
     $deployManifest = "$AppShortName.application"
@@ -59,9 +71,9 @@ function Publish-ClickOnce {
     $relativeVersionDir = "Application Files/$AppShortName" + "_$($version.Replace(".", "_"))"
     $versionDir = "$OutputDir/$relativeVersionDir"
     
-    Write-Verbose "Project Dir: $projectDir"
-    Write-Verbose "Output Dir: $OutputDir"
-    Write-Verbose "Version Dir: $versionDir"
+    Write-Host "Project Dir: $projectDir"
+    Write-Host "Output Dir: $OutputDir"
+    Write-Host "Version Dir: $versionDir"
 
     $appCodeBasePath = "$relativeVersionDir/$appManifest"
     $appManifestPath = "$versionDir/$appManifest"
@@ -69,31 +81,31 @@ function Publish-ClickOnce {
     $secondDeployManifestPath = "$versionDir/$deployManifest"
     $secondDeployUrl = "$DeploymentRootUrl/$relativeVersionDir/$deployManifest"
 
-    Write-Verbose "Appliction Manifest Path: $appManifestPath"
-    Write-Verbose "Deployment Manifest Path: $deployManifestPath"
-    Write-Verbose "Second Deployment Manifest Path: $secondDeployManifestPath"
+    Write-Host "Appliction Manifest Path: $appManifestPath"
+    Write-Host "Deployment Manifest Path: $deployManifestPath"
+    Write-Host "Second Deployment Manifest Path: $secondDeployManifestPath"
     
-    Write-Verbose "Checking if output folder already exists..."
+    Write-Host "Checking if output folder already exists..."
     if (Get-Item $versionDir -ErrorAction  SilentlyContinue) {
-        Write-Verbose "    Output folder already exists."
+        Write-Host "    Output folder already exists."
         if ($DeleteOutputDir) {
-            Write-Verbose "    Deleting existing output folder..."
+            Write-Host "    Deleting existing output folder..."
             Remove-Item -Recurse $versionDir
         } else {
-            Write-Verbose "    Include the -DeleteOutputDir parameter to automatically delete an existing output folder with the same name."
+            Write-Host "    Include the -DeleteOutputDir parameter to automatically delete an existing output folder with the same name."
             return
         }
     } else {
-         Write-Verbose "    Output folder does not already exist.  Continue."
+         Write-Host "    Output folder does not already exist.  Continue."
     }
 
-    Write-Verbose "Creating output folders..."
+    Write-Host "Creating output folders..."
     New-Item $versionDir -ItemType directory | Out-Null
  
-    Write-Verbose "Copying files into the output folder..."
+    Write-Host "Copying files into the output folder..."
     Copy-Item $Files -Destination $versionDir
     
-    Write-Verbose "Generating application manifest file: $appManifestPath"
+    Write-Host "Generating application manifest file: $appManifestPath"
     mage -New Application `
         -ToFile "$appManifestPath" `
         -Name $AppLongName `
@@ -102,9 +114,9 @@ function Publish-ClickOnce {
         -FromDirectory $versionDir `
         -TrustLevel FullTrust `
         -Algorithm $algorithm `
-        -IconFile $IconFile | Out-Null
+        -IconFile $IconFile | Out-Host
 
-    Write-Verbose "Adding file association to application manifest file ... "
+    Write-Host "Adding file association to application manifest file ... "
     [xml]$doc = Get-Content (Resolve-Path "$appManifestPath")
     $fa = $doc.CreateElement("fileAssociation")
     $fa.SetAttribute("xmlns", "urn:schemas-microsoft-com:clickonce.v1")
@@ -116,9 +128,9 @@ function Publish-ClickOnce {
     $doc.Save((Resolve-Path "$appManifestPath"))
 
     mage -Sign "$appManifestPath" `
-        -CertFile "$CertFile" | Out-Null
+        -CertFile "$CertFile" | Out-Host
 
-    Write-Verbose "Generating deployment manifest file: $deployManifestPath"
+    Write-Host "Generating deployment manifest file: $deployManifestPath"
     mage -New Deployment `
         -ToFile "$deployManifestPath" `
         -Name $AppLongName `
@@ -132,18 +144,18 @@ function Publish-ClickOnce {
         -ProviderURL "$DeploymentRootUrl/$deployManifest" `
         -Install true `
         -Publisher $Publisher `
-        -Algorithm $algorithm | Out-Null
+        -Algorithm $algorithm | Out-Host
 
-    Write-Verbose "Renaming files for web server deployment with .deploy..."
+    Write-Host "Renaming files for web server deployment with .deploy..."
     Get-ChildItem $versionDir | `
         Foreach-Object { `
             if (-not $_.FullName.EndsWith(".manifest")) { `
                 Rename-Item $_.FullName "$($_.FullName).deploy" } } 
 
-    Write-Verbose "Resigning application manifest..."
-    mage -Sign "$appManifestPath" -CertFile $CertFile | Out-Null
+    Write-Host "Resigning application manifest..."
+    mage -Sign "$appManifestPath" -CertFile $CertFile | Out-Host
     
-    Write-Verbose "Altering deployment manifest details..."
+    Write-Host "Altering inner  deployment manifest details..."
     $xml = [xml](Get-Content "$deployManifestPath")
     
     #Change application identiy name
@@ -159,33 +171,33 @@ function Publish-ClickOnce {
     $expirationNode.SetAttribute("maximumAge", "1")
     $expirationNode.SetAttribute("unit", "days")
     
-    Write-Verbose "Saving first altered deployment manifest..."
+    Write-Host "Saving altered inner deployment manifest..."
     $xml.Save("$deployManifestPath")
 
-    Write-Verbose "Signing first altered deployment manifest..."
+    Write-Host "Signing altered inner deployment manifest..."
     mage -Sign "$deployManifestPath" `
-        -CertFile $CertFile | Out-Null
+        -CertFile $CertFile | Out-Host
 
-    Write-Verbose "Loading deployment manifest to make second copy..."
+    Write-Host "Loading deployment manifest to make root copy..."
     $secondXml = [xml](Get-Content "$deployManifestPath")
 
-    Write-Verbose "Altering second manifest..."
+    Write-Host "Altering root deployment manifest..."
     $deploymentProviderNode = $secondXml.SelectSingleNode("//*[local-name() = 'deploymentProvider']")
     $deploymentProviderNode.SetAttribute("codebase", "$($secondDeployUrl.Replace(" ", "%20"))")
 
-    Write-Verbose "Saving second altered deployment manifest..."
+    Write-Host "Saving altered root deployment manifest..."
     $secondXml.Save("$secondDeployManifestPath")
 
-    Write-Verbose "Signing second altered deployment manifest..."
+    Write-Host "Signing altered root deployment manifest..."
     mage -Sign "$secondDeployManifestPath" `
-        -CertFile $CertFile | Out-Null
+        -CertFile $CertFile | Out-Host
 
-    Write-Verbose "ClickOnce deployment created."
-    Write-Verbose "Uploading files to Amazon Web Services S3..."
+    Write-Host "ClickOnce deployment created."
+    Write-Host "Uploading files to Amazon Web Services S3..."
     
-    Write-Verbose "Moving to Output Folder..."
+    Write-Host "Moving to Output Folder..."
     cd $OutputDir
-    Write-Verbose "Current Folder: $(Get-Location)"
+    Write-Host "Current Folder: $(Get-Location)"
 
     $publishFiles = dir $versionDir -Recurse -File
 
@@ -194,38 +206,24 @@ function Publish-ClickOnce {
     [System.Collections.ArrayList]$relativeFilePaths = @()
     foreach ($f in $publishFiles) {
         $relativeFilePath = "$($f.FullName.SubString($parentFolder.Length+1))"
-        Write-Verbose "$relativeFilePath"
         $relativeFilePaths.Add($relativeFilePath) | Out-Null
-        #Write-S3Object `
-        #    -BucketName $AmazonS3BucketName `
-        #    -Region $AmazonRegion `
-        #    -File $relativeFilePath `
-        #    -Key "$($relativeFilePath)" `
-        #    -CannedACLName $AmazonCannedACLName
     }
 
-    Write-Verbose "Last File:"
+    Write-Host "Last File:"
     $realDeployManifestPath = [System.IO.Path]::GetFullPath($deployManifestPath)
     $relativeFilePath = "$($realDeployManifestPath.SubString($parentFolder.Length+1))"
-    Write-Verbose "$relativeFilePath"
     $relativeFilePaths.Add($relativeFilePath) | Out-Null
-   # Write-S3Object `
-   #     -BucketName $AmazonS3BucketName `
-   #     -Region $AmazonRegion `
-   #     -File $relativeFilePath `
-   #     -Key "$($relativeFilePath)" `
-   #     -CannedACLName $AmazonCannedACLName
         
-    Write-Verbose "Current Folder: $(Get-Location)"
+    Write-Host "Current Folder: $(Get-Location)"
  
-    Write-Verbose "Moving back to project folder..."
-    popd
+    #Write-Host "Moving back to project folder..."
+    #popd
 
-    Write-Verbose "Current Folder: $(Get-Location)"
+    #Write-Host "Current Folder: $(Get-Location)"
     
-    Write-Verbose  "Saving Current Revision $revision to Revision.txt file..."
-    Set-Content -Value "$revision" -Path "./Revision.txt" -Encoding UTF8
-    Write-Verbose "Done."
+    Write-Host  "Saving Current Revision $revision to Revision.txt file..."
+    Set-Content -Value "$revision" -Path "$projectDir/Revision.txt" -Encoding UTF8
+    Write-Host "Done."
 
     return $relativeFilePaths
 }
