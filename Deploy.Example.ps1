6f58870bb3fcdc262cf39ea51bb5acd98a2fc8c2
Marc Durham <marcdurham@gmail.com>
2018-11-04 15:02:47 +0800

    Rename Deploy.Example.ps1

---
+++ Deploy.Example.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module AWSPowerShell
+Import-Module .\ClickOnce-Module.ps1
+
+#Build project
+"Building..."
+msbuild.exe AppName.WinForms.csproj /p:Configuration=Release /p:TargetVersion=4.0
+
+pushd .\
+cd ".\bin\Release"
+
+""
+# Optional Extra Commands Here
+# "Compressing UserManual folder..."
+# 7z a -sfx UserManual.exe UserManual
+
+""
+"Configuring files to deploy..." 
+#Configure file paths 
+#The first file is the "Entry Point" file, the main .exe.
+$files = `
+    "AppName.exe", `
+    "AppName.pdb", `
+    "AppName.exe.config", `
+    "AppNameIcon.ico", `
+    "UserManual.exe", `
+    "AppName.Common.dll", `
+    "AppName.Common.pdb"
+
+"Creating ClickOnce manifest files..." 
+Create-ClickOnce $files `
+    -AppProperName "Application Name" `
+    -AppShortName "AppName" `
+    -IconFilename "AppNameIcon.ico" `
+    -Publisher "Marc Durham" `
+    -OutputDir "../../../../ClickOnceDeploy" `
+    -CertFile "../../../AppName.pfx" `
+    -DeploymentUrl "http://appname.md9.us" `
+    -AmazonRegion "us-west-2" `
+    -BucketName "appname.md9.us" `
+	-FileExtension ".abc" `
+    -FileExtensionDescription "Application Name ABC File" `
+    -FileExtensionProgId "AppName.ABC" `
+    -ErrorAction Stop
+
+popd
